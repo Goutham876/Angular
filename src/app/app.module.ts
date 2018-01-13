60febd9fe964a8425dcb0ffa8a66a2a3451c6ef7
Gau <Goutham@Gouthams-MacBook-Air.local>
2018-01-13 16:41:59 +0530

    customPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { SummaryPipe } from './summary.pipe';
 import { CoursesService } from './courses.service';
 import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,10 +12,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,10 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template:`
-        <h2>{{title}}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{course}}
-            </li>
-        </ul>`
+        <h2>{{ text | summary:10 }}</h2>
+        `
 })
 
 export class CoursesComponent {
-    title= "list of courses";
-    courses;
-
-    constructor(service : CoursesService) {
-        this.courses = service.getCourses();
-    }
-
+    text = "It if sometimes furnished unwilling as additions so. Blessing resolved peculiar fat graceful ham. Sussex on at really ladies in as elinor. Sir sex opinions age properly extended. Advice branch vanity or do thirty living. Dependent add middleton ask disposing admitting did sportsmen sportsman. ";
 } 
