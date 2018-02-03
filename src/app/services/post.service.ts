3b02c00ca372fc62658bac9846b8abf3d9e6fb43
Gau <Goutham@Gouthams-MacBook-Air.local>
2018-02-03 16:51:41 +0530

    global error handle Refactor

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -9,35 +9,36 @@ import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class PostService {
-  private url = "https://jhgjsonplaceholder.typicode.com/posts";
+  private url = "https://jsonplaceholder.typicode.com/posts";
   constructor(private http : Http) { }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url).catch(this.handleError);
   }
 
   addPost(post) {
     return this.http.post(this.url,post)
-    .catch((error : Response) => {
-      if(error.status === 400)
-        return Observable.throw(new BadInput(error.json()));
-
-      return Observable.throw(new AppError(error));
-    });
+    .catch(this.handleError);
   }
 
   updatePost(post) {
-    return this.http.patch(this.url + '/' + post.id,JSON.stringify({ isRead : true}));
+    return this.http.patch(this.url + '/' + post.id,JSON.stringify({ isRead : true}))
+    .catch(this.handleError);
   }
 
   deletePost(post) {
     return this.http.delete(this.url + '/' + post.id)
-    .catch((error : Response) => {
-      if(error.status === 404)
+    .catch(this.handleError);
+  }
+
+  private handleError(error : Response) {
+    if(error.status === 400)
+        return Observable.throw(new BadInput(error.json()));
+
+    if(error.status === 404)
         return Observable.throw(new NotFoundError())
 
       return Observable.throw(new AppError(error))
-    });
   }
 
 }
